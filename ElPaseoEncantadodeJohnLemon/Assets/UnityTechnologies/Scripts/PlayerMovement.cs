7729bd3720bcc5330389a800d6b32d9a46407ca1
ArianaBet <arianabetsi2005@gmail.com>
2024-01-25 12:34:45 +0000

    .

--- ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{   public float turnSpeed = 20f;
+{
+    public float turnSpeed = 20f;
 
     Animator m_Animator;
+    Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    Rigidbody m_Rigidbody;
-    m_Rigidbody = GetComponent<Rigidbody>();
-
-    // Start is called before the first frame update
     void Start()
     {
-        m_Animator = GetComponent<Animator>(); 
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-
-        m_movement.Set(horizontal, 0f, vertical);
+        m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        bool IsWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.Setbool("IsWalking", isWalking); //Set bool es el metodo con dos parametros, uno del nombre del parametro Animator y otro del valor que quieres reestablecer para ese objeto
-
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
-    
-    
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
