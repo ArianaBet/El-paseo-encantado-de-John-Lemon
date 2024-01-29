a8152108b89a8b0eb99dd329aaac9ae93eae61ab
ArianaBet <arianabetsi2005@gmail.com>
2024-01-30 00:05:27 +0100

    .

--- ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/Observer.cs
+++ ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/Observer.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    public GameEnding gameEnding;
+    bool m_IsPlayerInRange;
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.transform == player)
+        {
+           m_IsPlayerInRange = true;
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            //V = P2 - P1 (vector = JohnLemon - PointOfView ) +vector3up(0,1,0) es para ver el centro de gravedad de John
+           
+            Ray ray = new Ray(transform.position, direction);
+
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+
+                }
+            }
+        }
+
         
     }
 }
