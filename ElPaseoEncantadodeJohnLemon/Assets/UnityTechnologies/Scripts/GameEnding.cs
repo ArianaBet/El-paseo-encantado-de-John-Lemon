ff64601a5798d72f31e7fc20f00a40ed608cb902
ArianaBet <arianabetsi2005@gmail.com>
2024-01-30 22:56:28 +0100

    .

--- ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ ElPaseoEncantadodeJohnLemon/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -1,40 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
-    public GameObject player;
-    public CanvasGroup exitBackgroundImageCanva;
-    bool m_IsPlayerAtExit;
-    float m_Timer;
     public float displayImageDuration = 1f;
-  
-    private void OnTriggerEnter(Collider other)
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
+          
         }
     }
 
-    private void Update()
+    public void CaughtPlayer()
     {
-       if (m_IsPlayerAtExit)
-        {
-            EndLevel();
-        } 
+        m_IsPlayerCaught = true;
     }
 
-    void EndLevel()
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+           
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanva.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-    if (m_Timer > fadeDuration + displayImageDuration)
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
+
 }
